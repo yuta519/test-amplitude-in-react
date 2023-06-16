29f7a4730c9a8714121a6fc1819e8e61c2e9658d
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-06-16 10:48:15 +0900

    Added mixpanel track functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { MouseEvent, useState } from "react";
+import mixpanel from "mixpanel-browser";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+const mixpanelId = import.meta.env.VITE_MIXPANEL_PROJECT_ID;
+mixpanel.init(mixpanelId, { debug: true });
+
+// Set this to a unique identifier for the user performing the event.
+// eg: their ID in your database or their email address.
+mixpanel.identify("ytkwmr18@gmail.com");
+
+// Track an event. It can be anything, but in this example, we're tracking a Signed Up event.
+// Include a property about the signup, like the Signup Type
+// mixpanel.track("Signed Up", {
+//   "Signup Type": "Referral",
+// });
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const handleClick = (event: MouseEvent<HTMLElement>) => {
+    mixpanel.track(`Clicked ${(event.target as Element).className}`, { count });
+  };
 
   return (
     <>
@@ -25,11 +43,24 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div className="card">
+        <button className="button-1" onClick={handleClick}>
+          Button 1
+        </button>
+      </div>
+      <div className="card">
+        <button className="button-2" onClick={handleClick}>
+          Button 2
+        </button>
+      </div>
+      <div className="card">
+        <button className="button-3" onClick={handleClick}>
+          Button 3
+        </button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
